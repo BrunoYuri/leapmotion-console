6ecfa8885173b2700a7e853c9acbf0bc4452f03e
U-james-laptop\james_000 <james_000@james-laptop.(none)>
2013-11-24 16:05:25 +0000

    added comments to the result model

--- LeapMotionTest/Result.cs
+++ LeapMotionTest/Result.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LeapMotionTest
 {
     public class Result
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public float XIntersect { get; set; }
-        public float YIntersect { get; set; }
-        public int ScreenWidth { get; set; }
+        public int X { get; set; } //Fingertip's X position in pixels
+        public int Y { get; set; } //Fingertip's Y position in pixels
+        public float XIntersect { get; set; } //Decimal of the finger's X position in relation to the screen
+        public float YIntersect { get; set; } //Decimal of the finger's Y position in relation to the screen
+        public int ScreenWidth { get; set; } 
         public int ScreenHeight { get; set; }
-        public float TipVelocity { get; set; }
+        public float TipVelocity { get; set; } //Speed at which the fingertip is moving
     }
 }
